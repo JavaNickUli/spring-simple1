0596a994173065d6ddef57b22bb499a438307504
JavaNickUli <java.nickuli@mail.ru>
2022-07-24 14:09:49 +0300

    UserController edit

--- src/main/java/uli/nick/spring/controller/UserController.java
+++ src/main/java/uli/nick/spring/controller/UserController.java
@@ -1,13 +1,29 @@
 package uli.nick.spring.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import uli.nick.spring.entity.UserEntity;
+import uli.nick.spring.repository.UserRepository;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public ResponseEntity registration(@RequestBody UserEntity user) {
+        try {
+            userRepository.save(user);
+            return ResponseEntity.ok("User saved successfully");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("An error has occurred");
+        }
+    }
 
     @GetMapping
     public ResponseEntity getUsers() {
