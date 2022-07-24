7be43292088a785369e5e3af63bb360e75929b96
JavaNickUli <java.nickuli@mail.ru>
2022-07-24 13:51:58 +0300

    Create UserController

---
+++ src/main/java/uli/nick/spring/controller/UserController.java
@@ -0,0 +1,20 @@
+package uli.nick.spring.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping
+    public ResponseEntity getUsers() {
+        try {
+            return ResponseEntity.ok("Server is running");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("An error has occurred");
+        }
+    }
+}
